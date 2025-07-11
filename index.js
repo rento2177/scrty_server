5a25b2f532e3f9f21eb8383efb6d55316068da39
Rento <109392019+rento2177@users.noreply.github.com>
2025-07-11 21:18:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 
-const server = http.createServer((req, res), () => {
+const server = http.createServer((_, res), () => {
   console.log("ok");
   res.end("ok");
 });
